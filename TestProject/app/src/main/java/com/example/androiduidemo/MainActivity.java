5435f4590ac9f63356836ebb9b8bf4f39df9dc7c
wanju <1874724133@qq.com>
2022-10-26 23:20:13 +0800

    init reporistory first

---
+++ TestProject/app/src/main/java/com/example/androiduidemo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.androiduidemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.caculator_layout);
+    }
+
+    public void acClick(View view) {
+
+    }
+
+    public void fourClick(View view) {
+    }
+}
